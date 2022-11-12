dc5cef3f6573bf7ea52c4817192bc9c0c054d46b
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-11-12 16:31:22 +0100

    Create User model

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,10 @@ module.exports = {
         "@typescript-eslint/no-empty-function": "off",
       },
     },
+    {
+      files: ["src/**/models/**.ts"],
+      rules: { "@typescript-eslint/naming-convention": "off" },
+    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
