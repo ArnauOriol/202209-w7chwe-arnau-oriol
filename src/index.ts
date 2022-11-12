5eaafa0e6c697f9a71605b9dfe083a134317ce43
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-11-12 13:37:11 +0100

    Add startServer function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+import environment from "./loadEnvirontments.js";
+import startServer from "./server/startServer.js";
 
+// eslint-disable-next-line no-implicit-coercion
+await startServer(+environment.port);
