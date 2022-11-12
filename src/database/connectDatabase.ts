46a359d4be39e90d05356af641b0464cc35e4c85
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-11-12 14:25:58 +0100

    Add databaseURL to loadEnvirontments

--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -2,7 +2,7 @@ import "../loadEnvirontments.js";
 import mongoose from "mongoose";
 import debugCreator from "debug";
 
-const debug = debugCreator("items:database:root");
+const debug = debugCreator("fakebook:database:root");
 
 const connectDatabase = async (mongoUrl: string) => {
   await mongoose.connect(mongoUrl);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import connectDatabase from "./database/connectDatabase.js";
 import environment from "./loadEnvirontments.js";
 import startServer from "./server/startServer.js";
 
 // eslint-disable-next-line no-implicit-coercion
 await startServer(+environment.port);
+await connectDatabase(environment.databaseUrl);
