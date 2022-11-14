57e1baa27dfd5e0b5428e5cd1a05303323027972
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-11-14 18:47:07 +0100

    Add loginUser to userControllers

--- src/loadEnvirontments.ts
+++ src/loadEnvirontments.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const environment = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGODB_URL,
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 export default environment;
--- src/server/types/types.ts
+++ src/server/types/types.ts
@@ -1,6 +1,18 @@
+import type { JwtPayload } from "jsonwebtoken";
+
 export interface RegisterData {
   username: string;
   password: string;
   email: string;
   picture?: string;
+}
+
+export interface UserTokenPayload extends JwtPayload {
+  id: string;
+  username: string;
+}
+
+export interface Credentials {
+  username: string;
+  password: string;
 }
