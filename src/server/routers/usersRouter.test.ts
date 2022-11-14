46eaefe69bd7b5a97f012df190cd1f48c32a1866
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-11-14 12:53:45 +0100

    Add registerUser error endpoint

--- src/server/routers/usersRouter.test.ts
+++ src/server/routers/usersRouter.test.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import bcrypt from "bcryptjs";
 import connectDatabase from "../../database/connectDatabase";
 import User from "../../database/models/User";
 import request from "supertest";
@@ -23,7 +24,7 @@ afterAll(async () => {
 });
 
 describe("Given the usersRouter with POST /signup endpoint", () => {
-  describe("When it receives a request with {username: 'Arnau', password: '1234', email: sr@arnau.com} on its body", () => {
+  describe("When it receives a request with {username: 'Arnau', password: '1234', email: sr@arnau.com} that is not on the database, on its body", () => {
     test("Then it should respond with status 201 and the message: 'User Arnau successfully created'", async () => {
       const requestBody: RegisterData = {
         username: "Arnau",
@@ -32,6 +33,30 @@ describe("Given the usersRouter with POST /signup endpoint", () => {
       };
       const expectedStatus = 201;
       const expectedMessage = { message: "User Arnau successfully created" };
+
+      const res = await request(app)
+        .post("/users/signup")
+        .send(requestBody)
+        .expect(expectedStatus);
+
+      expect(res.body).toStrictEqual(expectedMessage);
+    });
+  });
+
+  describe("When it receives a request with {username: 'Yo', password: '33', email: y@o.com} that is on the database, on its body", () => {
+    test("Then it should respond with", async () => {
+      await User.create({
+        username: "Yo",
+        password: await bcrypt.hash("33", 10),
+        email: "y@o.com",
+      });
+      const requestBody: RegisterData = {
+        username: "Yo",
+        password: "33",
+        email: "y@0.com",
+      };
+      const expectedStatus = 500;
+      const expectedMessage = { error: "Error on registration" };
 
       const res = await request(app)
         .post("/users/signup")
